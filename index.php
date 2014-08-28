924190be272269e7ef19b6b21c233c2832b80981
telesduf1 <fabriciotdg@gmail.com>
2014-08-28 12:58:36 -0400

    This was edited at Aptana Studio

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+
+		<title>HTML</title>
+
+	</head>
+
+	<body>
+		<div>
+			<header>
+				<h1>This was edited at c9</h1>
+			</header>
+			<nav>
+				<p>
+					<a href="/">Home</a>
+				</p>
+				<p>
+					<a href="/contact">Contact</a>
+				</p>
+			</nav>
+
+			<div>
+
+			</div>
+
+			<footer>
+				<p>
+					&copy; Copyright  by telesduf1
+				</p>
+			</footer>
+		</div>
+	</body>
+</html>
